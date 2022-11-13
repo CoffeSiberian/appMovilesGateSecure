fa919d9df3f927e92c6c36e2425e905133b9cf66
SiberianCoffe <siberiancoffe@outlook.cl>
2022-11-13 14:30:58 -0300

    user card material 3

--- app/src/main/java/com/example/gatesecure/crudActivity.kt
+++ app/src/main/java/com/example/gatesecure/crudActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class crudActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crud)
