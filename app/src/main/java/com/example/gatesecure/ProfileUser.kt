3010dc85e55ce1507f85c70ab970ed918cdeb766
SiberianCoffe <siberiancoffe@outlook.cl>
2022-11-13 16:35:38 -0300

    material BottomAppBar

--- app/src/main/java/com/example/gatesecure/ProfileUser.kt
+++ app/src/main/java/com/example/gatesecure/ProfileUser.kt
@@ -9,55 +9,38 @@ import com.google.firebase.auth.FirebaseAuth
 
 class ProfileUser : AppCompatActivity() {
 
-    //view binding
     private lateinit var binding: ActivityProfileUserBinding
-
-    //ActionBar
     private lateinit var actionBar: ActionBar
-
-    //Firebase Auth
     private lateinit var firebaseAuth: FirebaseAuth
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //configuracion de la barra de acciones
         actionBar = supportActionBar!!
         actionBar.title = "Perfil"
 
-        //Verificar si el usuario esta logueado
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
 
-        //handle click, cerrar sesion
         binding.btnLogout.setOnClickListener {
             firebaseAuth.signOut()
             checkUser()
         }
 
-        //ir al crud
         binding.btnCRUD.setOnClickListener {
             startActivity(Intent(this, CrudActivity::class.java))
         }
-
-
     }
 
     private fun checkUser() {
-        //obtener usuario actual
         val firebaseUser = firebaseAuth.currentUser
         if (firebaseUser != null){
-            //usuario esta logueado
-            //obtener email del usuario
             val email = firebaseUser.email
-            //setear email en textview
             binding.tvEmail.text = email
         }
         else{
-            //usuario no esta logueado, ir a la actividad main
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
--- app/src/main/java/com/example/gatesecure/ViewUsers.kt
+++ app/src/main/java/com/example/gatesecure/ViewUsers.kt
@@ -3,7 +3,7 @@ package com.example.gatesecure
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class viewUsers : AppCompatActivity() {
+class ViewUsers : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_users)
